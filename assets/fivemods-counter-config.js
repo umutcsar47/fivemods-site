90aa30e5d1a46bfc0f41955abb4e13936563e54a
umutcsar47 <umutcansincar@gmail.com>
2026-03-21 19:30:04 +0300

    Site guncellemesi

---
+++ assets/fivemods-counter-config.js
@@ -0,0 +1,6 @@
+﻿window.FiveModsCounterConfig = window.FiveModsCounterConfig || {
+  // Cloudflare Worker yayinlandiktan sonra buraya workers.dev adresini yazin.
+  workerBaseUrl: "https://fivemods-download-counter.umutcansincar.workers.dev",
+  statsRefreshMs: 15000
+};
+
