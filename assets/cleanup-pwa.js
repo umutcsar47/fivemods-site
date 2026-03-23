49baa12ea1f978e971639bc472ba2d28e27c4622
umutcsar47 <umutcansincar@gmail.com>
2026-03-23 19:17:38 +0300

    Site guncellemesi

---
+++ assets/cleanup-pwa.js
@@ -0,0 +1,27 @@
+﻿(function () {
+  try {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        registrations.forEach(function (registration) {
+          registration.unregister();
+        });
+      }).catch(function () {});
+    }
+
+    if (window.caches && caches.keys) {
+      caches.keys().then(function (keys) {
+        keys.forEach(function (key) {
+          if (/fivemods|workbox|precache|runtime/i.test(key)) {
+            caches.delete(key);
+          }
+        });
+      }).catch(function () {});
+    }
+
+    document.documentElement.classList.remove('pwa-installed');
+    if (document.body) {
+      document.body.classList.remove('pwa-installed');
+    }
+  } catch (error) {
+  }
+})();
